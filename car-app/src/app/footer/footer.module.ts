33b45613b86c3dc4ed6c0bbe72e971e26568fb15
Rustam Tazyaev <rustytazyaev@gmail.com>
2022-01-31 17:03:10 +0400

    dev-info_v1

---
+++ car-app/src/app/footer/footer.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DevInfoComponent } from './dev-info/dev-info.component';
+
+
+
+@NgModule({
+  declarations: [
+    DevInfoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FooterModule { }
